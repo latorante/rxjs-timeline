3c77d0f2422f2b94cd55e4371d87bed85dc27ee8
Martin Picha <mpicha@atlassian.com>
2019-07-17 15:45:46 +1000

    Add body cursor helper functions

--- src/reactive/utils.ts
+++ src/reactive/utils.ts
@@ -46,28 +46,56 @@ export function filterMouseEvents(
 /**
  * Fallback CSS
  */
-const grabbingCursor: string = ';cursor: grabbing;';
+export const grabbingCursor: string = ';cursor: grabbing;';
+export const resizeCursorLeft: string = ';cursor: w-resize;';
+export const resizeCursorRight: string = ';cursor: e-resize;';
+
+/**
+ * Get Cursor helper is feeded a MouseEvent
+ * and figures what the cursor will be on the document.
+ * 
+ * We use this to simulate the cursor while you drag / resize on an element
+ * while React re-draws the element underneath, which forces in normal behavior
+ * the element to loose the cursor.
+ */
+export getCursor(event: any): string {
+  if(!event){
+    return '';
+  }
+  const target: HTMLElement = event.target;
+  const targetType: MovementType = getElementType(target);
+  /**
+   * Exit early, if we're dragging, we don't need to
+   * figure out the direction and just return grabbing cursor.
+   */
+  if(targetType === MovementType.Drag){
+    return grabbingCursor;
+  }
+  const targetDirection: MovementType = getElementDirectionFrom(target);
+  return targetDirection === MovementType.Left ? resizeCursorLeft : resizeCursorRight;
+}
 
 /**
  * Helpers with grabbing elements styling
- * - This sets curost to grab on start drag / resize and move
+ * - This sets cursor to grab on start drag / resize and move
  */
 export function setStartCursor(event: any): void {
-  const target: HTMLElement = event.target;
-  const targetType: MovementType = getElementType(target);
-  const targetDirection: MovementType = getElementDirectionFrom(target);
-  if (targetType && targetDirection) {
+  if(!event){
+    return;
   }
+  const cursor = getCursor(event);
   const style = document.body.getAttribute('style') as string;
-  document.body.setAttribute('style', style + grabbingCursor);
+  document.body.setAttribute('style', style + cursor);
 }
 
 /**
  * This resets back the original value
  */
 export function setEndCursor(event: any): void {
-  if (event) {
+  if(!event){
+    return;
   }
+  const cursor = getCursor(event);
   const style = document.body.getAttribute('style') as string;
-  document.body.setAttribute('style', style.replace(grabbingCursor, ''));
+  document.body.setAttribute('style', style.replace(cursor, ''));
 }
