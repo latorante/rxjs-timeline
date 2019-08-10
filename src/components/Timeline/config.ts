d1b34b479f51aac5ca9ed121f5429aba00994e0d
Martin Picha <martin@latorante.name>
2019-08-10 20:07:18 +1000

    Add code comments

---
+++ src/components/Timeline/config.ts
@@ -0,0 +1,9 @@
+import { TimelineProps } from './declarations';
+
+export const defaultProps: TimelineProps = {
+  numberOfColumns: 1,
+  withHeader: () => null,
+  stripped: true,
+};
+
+export default defaultProps;
